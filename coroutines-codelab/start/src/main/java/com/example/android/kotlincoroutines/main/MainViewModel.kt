b436cbf73dce2a4366e650768d366218810f840b
ivenzhang <ivenzhang@tencent.com>
2024-09-14 16:56:08 +0800

    提交修改后的代码

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -16,11 +16,16 @@
 
 package com.example.android.kotlincoroutines.main
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.BACKGROUND
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
@@ -33,6 +38,7 @@ import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
 class MainViewModel(private val repository: TitleRepository) : ViewModel() {
 
     companion object {
+        val TAG = "zxw.MainViewModel"
         /**
          * Factory for creating [MainViewModel]
          *
@@ -101,11 +107,13 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      * Wait one second then update the tap count.
      */
     private fun updateTaps() {
-        // TODO: Convert updateTaps to use coroutines
-        tapCount++
-        BACKGROUND.submit {
-            Thread.sleep(1_000)
-            _taps.postValue("${tapCount} taps")
+        viewModelScope.launch {
+            tapCount++
+            Log.d(TAG, "viewModelScope launch 0 treadName: ${Thread.currentThread().name}" )
+            // 此处的 delay 并不会阻塞当前线程（也就是主线程）
+            delay(1000)
+            Log.d(TAG, "viewModelScope launch 1 treadName: ${Thread.currentThread().name}" )
+            _taps.postValue("$tapCount taps")
         }
     }
 
@@ -119,18 +127,38 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
     /**
      * Refresh the title, showing a loading spinner while it refreshes and errors via snackbar.
      */
-    fun refreshTitle() {
-        // TODO: Convert refreshTitle to use coroutines
-        _spinner.value = true
-        repository.refreshTitleWithCallbacks(object : TitleRefreshCallback {
-            override fun onCompleted() {
-                _spinner.postValue(false)
+    private fun refreshTitle() {
+        // 使用协程还有一个好处是，scope一般是挂在生命周期上，会自动处理退出
+        // 由于我们使用了 viewModelScope，因此，当用户离开此屏幕时，此协程启动的操作将自动取消。
+        // 这意味着它不会发出其他网络请求或数据库查询。
+        viewModelScope.launch {
+            try {
+                _spinner.value = true
+                repository.refreshTitle()
+            } catch (error: TitleRefreshError) {
+                _snackBar.value = error.message
+            } finally {
+                _spinner.value = false
             }
+        }
+    }
 
-            override fun onError(cause: Throwable) {
-                _snackBar.postValue(cause.message)
-                _spinner.postValue(false)
+    private fun launchDataLoad(block : suspend () -> Unit): Job {
+        return viewModelScope.launch {
+            try {
+                _spinner.value = true
+                block()
+            } catch (error: TitleRefreshError) {
+                _snackBar.value = error.message
+            } finally {
+                _spinner.value = false
             }
-        })
+        }
+    }
+
+    fun refreshTitleV2() {
+        launchDataLoad {
+            repository.refreshTitle()
+        }
     }
 }
